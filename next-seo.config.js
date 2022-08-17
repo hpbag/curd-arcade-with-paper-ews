7d1eec7d35ea1350cdb37f2cccd1c043a05b9497
ElasticBottle <winstonyeo99@yahoo.com>
2022-08-16 17:02:42 -0700

    Fix link share image

--- next-seo.config.js
+++ next-seo.config.js
@@ -11,7 +11,7 @@ const defaultSEOConfig = {
     description: "Pay to Play, win real money",
     images: [
       {
-        url: "https://og-image.sznm.dev/**nextarter-chakra**.sznm.dev.png?theme=dark&md=1&fontSize=125px&images=https%3A%2F%2Fsznm.dev%2Favataaars.svg&widths=250",
+        url: "https://curdinc.com/android-chrome-192x192.png",
         alt: "curdinc.com og-image",
       },
     ],
